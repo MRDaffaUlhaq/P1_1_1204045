6a6f469054855b498dd952e3523f47ff7e96bbaf
daffaulhaq <udaffa09@gmail.com>
2021-11-05 21:31:04 +0700

    upload file program

---
+++ P1_1_1204045/P1_1_1204045/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_1_1204045
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I'm Daffa from 2B 1204045");
+        }
+    }
+}
